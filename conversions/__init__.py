c2f20960a92de66ca83a1683a7ff50f697dff3bc
Norman Liu <normanwuweiliu@gmail.com>
2020-09-28 00:39:58 -0700

    Major refactoring with a focus on scalability. Conversions object now has methods to convert between any SI prefix of the same unit.

--- conversions/__init__.py
+++ conversions/__init__.py
@@ -1,25 +1,83 @@
+"""Creates a Conversions object. The convert method takes a value, \
+        an optional SI prefix string to convert from, and an \
+        SI suffix string to convert to."""
+
 class Conversions:
+    def __init__(self):
+        prefixes = [
+                "y",
+                "z",
+                "a",
+                "f",
+                "p",
+                "n",
+                "u",
+                "m",
+                "c",
+                "d",
+                "",
+                "da",
+                "h",
+                "k",
+                "M",
+                "G",
+                "T",
+                "P",
+                "E",
+                "Z",
+                "Y",
+                ]
+        inverse_exps = [
+                24,
+                21,
+                18,
+                15,
+                12,
+                9,
+                6,
+                3,
+                2,
+                1,
+                0,
+                -1,
+                -2,
+                -3,
+                -6,
+                -9,
+                -12,
+                -15,
+                -18,
+                -21,
+                -24
+                ]
+        self.prefices = dict(
+                            zip(
+                                prefixes,
+                                map(lambda x: 10 ** x, inverse_exps)
+                            )
+                        )
 
-  # Converts kilometers to meters
-  def km_to_m(self, km):
-    return km * 1_000
-  
-  # Converts meters to kilometers
-  def m_to_km(self, m):
-    solution = m / 1_000
-    return solution
+    def convert(self, val: int, base: str = "", target: str = "") -> int:
+        if base not in self.prefices or target not in self.prefices:
+            raise Exception("Not a valid SI unit.")
+        return val * (self.prefices[target] / self.prefices[base])
 
-  # Converts milligrams to grams
-  def mg_to_g(self, mg):
-    solution = mg / 1_000
-    return solution
+    # Converts kilometers to meters
+    def km_to_m(self, km):
+        return self.convert(km, 'k')
 
-  # Converts milligrams to kilograms
-  def mg_to_kg(self, mg):
-    solution = mg / 1_000_000
-    return solution
+    # Converts meters to kilometers
+    def m_to_km(self, m):
+        return self.convert(m, target='k')
 
-  # Converts millimeters to kilometers
-  def mm_to_km(self, mm):
-    solution = mm / 1_000_000
-    return solution
+    # Converts milligrams to grams
+    def mg_to_g(self, mg):
+      return self.convert(mg, 'm')
+
+    # Converts milligrams to kilograms
+    def mg_to_kg(self, mg):
+      return self.convert(mg, 'm', 'k')
+
+    # Converts millimeters to kilometers
+    def mm_to_km(self, mm):
+        return self.convert(mm, 'm', 'k')
